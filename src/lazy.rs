b8b24f0f7e6305b20050ea7da8461c4ab1d22cfa
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2017-05-06 16:45:26 +0900

    Add lazy macro

--- src/lazy.rs
+++ src/lazy.rs
@@ -1,25 +1,55 @@
-// Inspired by: https://github.com/reem/rust-lazy/
+use std::boxed::Box;
 
-pub enum Thunk<'a, T: 'a> {
-    Todo { thunk: &'a FnOnce() -> T },
-    Done { value: T },
+pub struct Thunk<'a, T: 'a + Clone> {
+    thunk: Box<Fn() -> T + 'a>,
+    value: Option<T>,
 }
 
-impl<'a, T: 'a> Thunk<'a, T> {
-    pub fn new(t: &'a FnOnce() -> T) -> Self {
-        Thunk::Todo { thunk: t }
+impl<'a, T: Clone> Thunk<'a, T> {
+    pub fn new<F>(t: F) -> Self
+        where F: Fn() -> T + 'a {
+        Thunk {
+            thunk: Box::new(t),
+            value: None }
     }
 
-    pub fn eval(&mut self) -> &T {
-        let value = match self {
-            &mut Thunk::Done { ref value } => value,
-            &mut Thunk::Todo { ref thunk } => &thunk()
-        };
-        value
+    pub fn eval(&mut self) -> T {
+        match self.value {
+            Some(ref value) => value.clone(),
+            None => {
+                let value = (self.thunk)();
+                self.value = Some(value.clone());
+                value
+            }
+        }
     }
 }
 
 #[macro_export]
 macro_rules! lazy {
     ($e:expr) => { Thunk::new(move || { $e }) }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static mut V: i32 = 7;
+    fn inc() -> i32 {
+        unsafe {
+            V += 1;
+            V
+        }
+    }
+
+    #[test]
+    fn test_lazy() {
+        let mut result = lazy!(2 + 3);
+        assert_eq!(5, result.eval());
+        assert_eq!(5, result.eval());
+
+        let mut result = lazy!(inc());
+        assert_eq!(8, result.eval());
+        assert_eq!(8, result.eval());
+    }
 }
